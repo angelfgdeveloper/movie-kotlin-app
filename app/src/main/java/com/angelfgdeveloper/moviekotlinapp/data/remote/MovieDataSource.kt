8084bf99221e2473dff30e8c55844c51f6aa3f86
Luis Angel <angelfgdeveloper@gmail.com>
2021-11-12 12:13:41 -0600

    Capa de datos

--- app/src/main/java/com/angelfgdeveloper/moviekotlinapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/angelfgdeveloper/moviekotlinapp/data/remote/MovieDataSource.kt
@@ -1,4 +1,19 @@
 package com.angelfgdeveloper.moviekotlinapp.data.remote
 
+import com.angelfgdeveloper.moviekotlinapp.data.model.MovieList
+
 class MovieDataSource {
+
+    fun getUpcomingMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getTopRatedMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getPopularMovies(): MovieList {
+        return MovieList()
+    }
+
 }
