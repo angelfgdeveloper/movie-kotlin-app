caeff8e5f91287b73998dd3dcad2b22fd88d4417
Luis Angel <angelfgdeveloper@gmail.com>
2021-11-11 20:05:48 -0600

    implementación de ViewBinding

--- app/src/main/java/com/angelfgdeveloper/moviekotlinapp/MovieDetailFragment.kt
+++ app/src/main/java/com/angelfgdeveloper/moviekotlinapp/MovieDetailFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.angelfgdeveloper.moviekotlinapp.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
 
+    private lateinit var binding: FragmentMovieDetailBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieDetailBinding.bind(view)
     }
 
 }
--- app/src/main/java/com/angelfgdeveloper/moviekotlinapp/MovieFragment.kt
+++ app/src/main/java/com/angelfgdeveloper/moviekotlinapp/MovieFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.angelfgdeveloper.moviekotlinapp.databinding.FragmentMovieBinding
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
 
+    private lateinit var binding: FragmentMovieBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieBinding.bind(view)
     }
 
 }
