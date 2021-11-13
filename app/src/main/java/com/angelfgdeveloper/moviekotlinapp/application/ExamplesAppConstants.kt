3bb73612699a215dbc6b28cf1948bcac31c0fa52
Luis Angel <angelfgdeveloper@gmail.com>
2021-11-12 23:05:35 -0600

    Excluyendo AppConstants

---
+++ app/src/main/java/com/angelfgdeveloper/moviekotlinapp/application/ExamplesAppConstants.kt
@@ -0,0 +1,10 @@
+package com.angelfgdeveloper.moviekotlinapp.application
+
+object ExamplesAppConstants {
+
+    // API
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = ""
+
+    const val API_LANGUAGE = "es-ES"
+}
